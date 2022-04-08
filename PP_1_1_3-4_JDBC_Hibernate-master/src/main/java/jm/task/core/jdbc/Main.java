be13769d003c54aeeb6a5446e5ccf42b49df2749
Ivan Black <black.ivan1990@gmail.com>
2022-04-08 23:33:50 +0300

    Hiber

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,19 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao user = new UserDaoJDBCImpl();
+        UserDao user = new UserDaoHibernateImpl();
         user.createUsersTable();
-        user.saveUser("Ivan", "Black", (byte) 32);
-        user.saveUser("Old", "Man", (byte) 90);
-        user.saveUser("Mark", "Ivanovich", (byte) 1);
-        user.saveUser("Ilona", "Konenko", (byte) 19);
-        user.removeUserById(1);
-        user.getAllUsers();
+   //     user.saveUser("Ivan", "Black", (byte) 32);
+   //     user.saveUser("Old", "Man", (byte) 90);
+   //     user.saveUser("Mark", "Ivanovich", (byte) 1);
+   //     user.saveUser("Ilona", "Konenko", (byte) 19);
+   //     user.removeUserById(1);
+   //     user.getAllUsers();
 
 
    //     user.cleanUsersTable();
--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -12,31 +17,93 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.UtilHibernate.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            Query query = session.createSQLQuery(" CREATE TABLE IF NOT EXISTS users " +
+                    "(id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(30), lastName VARCHAR(20), " +
+                    "age TINYINT)").addEntity(User.class);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.UtilHibernate.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query query = session.createSQLQuery("DROP TABLE IF EXISTS users").addEntity(User.class);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try (Session session = Util.UtilHibernate.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try (Session session = Util.UtilHibernate.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User userForDelete = session.get(User.class, id);
+            session.delete(userForDelete);
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.UtilHibernate.getSessionFactory().openSession()) {
+            return session.createQuery("from User", User.class).list();
+        }
+
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.UtilHibernate.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query query = session.createSQLQuery("delete from users").addEntity(User.class);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
